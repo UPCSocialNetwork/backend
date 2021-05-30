f8a080be6e98d85101ba4dd56fe16438197f530b
1Woody <[daniel.benavente4@gmail.com]>
2021-05-31 00:47:38 +0200

    new route assig

--- src/controllers/Assignatura/getGrau.ts
+++ src/controllers/Assignatura/getGrau.ts
@@ -2,14 +2,16 @@ import { RequestHandler } from 'express';
 import requestMiddleware from '../../middleware/request-middleware';
 import Assignatura, { IAssignatura } from '../../models/Assignatura';
 
-const all: RequestHandler = async (req, res) => {
+const getGrau: RequestHandler = async (req, res) => {
+  const { grauID } = req.body;
   let assignatura: IAssignatura[] = null;
   try {
-    assignatura = await Assignatura.find();
+    assignatura = await Assignatura.find({ grauID });
   } catch (e) {
-    return res.send({ message: e });
+    return res.send({ e });
   }
+  if (!assignatura) return res.send({ message: 'Assignatura not found' });
   return res.send({ assignatura });
 };
 
-export default requestMiddleware(all);
+export default requestMiddleware(getGrau);
--- src/controllers/Assignatura/index.ts
+++ src/controllers/Assignatura/index.ts
@@ -1,13 +1,8 @@
 import add from './add';
 import getAll from './getAll';
 import getOne from './getOne';
+import getGrau from './getGrau';
 import update from './update';
 import deleteAssignatura from './delete';
 
-export {
-  add,
-  getAll,
-  getOne,
-  update,
-  deleteAssignatura
-};
+export { add, getAll, getOne, getGrau, update, deleteAssignatura };
