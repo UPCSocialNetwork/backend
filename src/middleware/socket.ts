d77c3ad2164fade121b77974e220de3ea8ac894e
cguti8 <infocesarguti@gmail.com>
2021-06-17 19:59:29 +0200

    socket tests

--- src/middleware/socket.ts
+++ src/middleware/socket.ts
@@ -7,6 +7,7 @@
 module.exports = (
   app: any,
   io: {
+    sockets: { adapter: { rooms: any } };
     on: (arg0: string, arg1: (socket: any) => void) => void;
     emit: (arg0: string, arg1: any) => void;
     to: (arg0: any) => { (): any; new (): any; emit: { (arg0: string, arg1: any, arg2: any): void; new (): any } };
@@ -15,6 +16,7 @@ module.exports = (
   io.on('connection', (socket) => {
     socket.on('xat actiu', (room: any) => {
       socket.join(room);
+      console.log(io.sockets.adapter.rooms);
     });
 
     socket.on('send message', (message: any, roomID: any) => {
@@ -22,16 +24,30 @@ module.exports = (
     });
 
     socket.on('listXat ready', (nom: any) => {
-      // console.log('Usuari: ' + nom);
+      // console.log(nom + ' ready');
       socket.join(nom);
-      // io.to(nom).emit('refresh list', )
+      socket.leave(socket.id);
+      console.log(io.sockets.adapter.rooms);
+      io.to(nom).emit('listening', true, true);
     });
 
     socket.on('refresh list', (message: any, nom: any, roomID: any) => {
       // console.log('refresca socio');
       // console.log('nom: ' + nom);
-      // console.log(socket.rooms);
-      io.to(nom).emit('update message', message, roomID);
+      console.log(io.sockets.adapter.rooms.get(nom).size);
+      // console.log(socket.adapter.rooms);
+      // console.log('--------------');
+      // console.log(socket.adapter.sids);
+      socket.to(nom).emit('update message', message, roomID);
+      // io.to(nom).emit('listening', true, true);
+    });
+    socket.on('leave', (roomID: any) => {
+      socket.leave(roomID);
+      console.log(io.sockets.adapter.rooms);
+    });
+    socket.on('disconnect', (reason: string) => {
+      console.log(reason);
+      socket.leave(socket.id);
     });
   });
 };
