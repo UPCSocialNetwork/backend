d2a9bc54e79f71ddd06ea7ba9a84228d9c296e4a
1Woody <[daniel.benavente4@gmail.com]>
2021-06-26 19:07:04 +0200

    getOne new XatAssig

--- src/controllers/XatAssignatura/index.ts
+++ src/controllers/XatAssignatura/index.ts
@@ -2,7 +2,8 @@ import add from './add';
 import getXatAssig from './getXatAssig';
 import getAll from './getAll';
 import getOne from './getOne';
+import getOneID from './getOneID';
 import update from './update';
 import deleteXatAssignatura from './delete';
 
-export { add, getXatAssig, getAll, getOne, update, deleteXatAssignatura };
+export { add, getXatAssig, getAll, getOne, getOneID, update, deleteXatAssignatura };
